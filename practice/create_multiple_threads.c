51409ecea01b59316b6a5ecf606eb780a1de672f
NhungNg <nnhung2@gmail.com>
2021-08-05 12:16:06 +0200

    Second commits

---
+++ practice/create_multiple_threads.c
@@ -0,0 +1,42 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct sum_s
+{
+    long long limit;
+    long long answer;
+
+} sum_t;
+
+void *sum_runner(void *arg)
+{
+    sum_t *arg_struct = (sum_t *)arg;
+
+    long long sum = 0;
+    for (long long i = 0; i < arg_struct->limit; i++)
+        sum += i;
+    arg_struct->answer = sum;
+    pthread_exit(0);
+}
+
+int     main(int ac, char **av)
+{
+    int num_arg =  ac - 1;
+    sum_t sum_arg[num_arg];
+    pthread_t thread_ids[num_arg];
+
+    if (ac < 2)
+        return (0);
+    for (int i = 0; i < num_arg; i++)
+    {
+        sum_arg[i].limit = atoll(av[i + 1]);
+        pthread_create(&thread_ids[i], NULL, sum_runner, &sum_arg[i]);
+    }
+    for (int i = 0; i < num_arg; i++)
+    {
+        pthread_join(thread_ids[i], NULL);
+        printf("Sum for thread %d is %lld\n", i, sum_arg[i].answer);
+    }
+    return (0);
+}
--- practice/try_pthread.c
+++ practice/try_pthread.c
@@ -3,28 +3,26 @@
 #include <unistd.h>
 #include <pthread.h>
 
-int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-//each thread carray a number.
 
-void    *routine(void *arg)
+void    *routine(void *tid)
 {
-    sleep(1);
-    int index = *(int*)arg;
-    printf("%d ", primes[index]);
 
+    long *my_id;
+
+    my_id = (long *)tid;
+    printf("This is thread: %ld \n", *my_id);
 }
 
 int main(int ac, char **av)
 {
-    pthread_t th[10];
-    for (int i = 0; i < 10; i++)
+    pthread_t tid[3];
+    int i;
+
+    i = 0;
+    while (i < 3)
     {
-        if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
-            perror("Fail to create thread");
+        pthread_create(&tid[i], NULL, &routine, (void *)&tid[i]);
+        i++;
     }
-    for (int i = 0; i < 10; i++)
-    {
-        if (pthread_join(th[i], NULL) != 0) //wait for thread
-            perror("Fail to create thread");
-    }
+    pthread_exit(NULL); // wait until pthread_creat finishs, execute all codes inside routin.
 }
